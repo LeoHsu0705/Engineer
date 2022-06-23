3a5381770297ecfa203eb4e26eca77fd6a248d7d
leo_hsu <leo@super-royal.com>
2022-06-23 15:21:37 +0800

    SHELL

--- Bin/yaml-bulid/stevencode.sh
+++ Bin/yaml-bulid/stevencode.sh
@@ -2,12 +2,12 @@
 
 file=stevencode-setting.txt
 num=`cat stevencode-setting.txt|grep -v "#"|wc -l`
-game=gcs
+read -p "please enter the update template gs or gcs：" game
 
 for (( i=1;i<=$num;i++ ))
         do
                 StevenCode=`cat $file |grep -v "#" |head -$i|tail -1|cut -d ',' -f1`
-                image=DEV20220621
+                image=DEV20220624
                 id="$game-$StevenCode"
                 cp -f $game-deploy.yaml ../../deployment/$game/$id-deploy.yaml
                 sed -i '' "s/tmp-deploy/$id/g"  ../../deployment/$game/$id-deploy.yaml
